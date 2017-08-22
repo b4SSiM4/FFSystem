e2b1c47f78ee6d4aa6f0a7bef7c05dbddab2eb59
b4SSiM4 <b4SSiM4@gmail.com>
2017-08-22 16:31:54 +0200

    Minor changes on actual project root

--- FFSystem-v0.1/src/pages/home/home.ts
+++ FFSystem-v0.1/src/pages/home/home.ts
@@ -12,3 +12,38 @@ export class HomePage {
   }
 
 }
+//What I added  is below
+
+import { ActionSheetController } from 'ionic-angular';
+
+export class MyPage {
+  constructor(public actionSheetCtrl: ActionSheetController) {
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+}
